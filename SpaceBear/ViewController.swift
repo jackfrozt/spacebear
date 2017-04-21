322446b26a6dfc410df19bea1559fd5616291506
Jacky Li <jackyli@Jackys-MacBook-Pro.local>
2017-04-20 22:18:30 -0400

    Initial Commit

---
+++ SpaceBear/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpaceBear
+//
+//  Created by Jacky Li on 2017-04-20.
+//  Copyright © 2017 Jacky Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
